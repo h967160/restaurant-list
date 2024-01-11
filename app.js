6f9601abbbbf9bc37707364455977481f347bc25
Huang <h96716@gmail.com>
2024-01-11 18:11:33 +0800

    feat: add search funtion to listing

--- app.js
+++ app.js
@@ -4,19 +4,36 @@ const app = express();
 const port = 3000;
 const restaurants = require("./public/jsons/restaurant.json").results;
 
+//setting template engine
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
+
+// setting static files
 app.use(express.static("public"));
 
+//setting routes
+
+// index route
 app.get("/", (req, res) => {
   res.redirect("/restaurants");
 });
-
 app.get("/restaurants", (req, res) => {
   res.render("index", { restaurants: restaurants });
 });
 
+// search route
+app.get("/search", (req, res) => {
+  const keywords = req.query.keyword?.trim().toLowerCase();
+  const matchedRestaurants = restaurants.filter(
+    (rest) =>
+      rest.name.toLowerCase().includes(keywords) ||
+      rest.category.toLowerCase().includes(keywords)
+  );
+  res.render("index", { restaurants: matchedRestaurants, keywords });
+});
+
+// detail route
 app.get("/restaurant/:id", (req, res) => {
   const id = req.params.id;
   const restaurant = restaurants.find((rest) => rest.id.toString() === id);
